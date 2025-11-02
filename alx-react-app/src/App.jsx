809f97d024a4dd4d52f61cf1387484c03c3206c7
Oluwabukunmi Moronfolu <kumzyd@gmail.com>
2025-11-02 13:46:17 +0100

    app updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,11 @@
-import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,8 +22,12 @@ function App() {
       </div>
 
       <h1>Vite + React</h1>
-      {/* 👇 Add your new component here */}
+
+      {/* 👇 Custom Components */}
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
