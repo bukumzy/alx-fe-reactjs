cc5ead816b80465ce1a31b99f387d171c757333d
bukumzy <kumzyd@gmail.com>
2025-11-21 00:13:47 +0100

    new react app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: '20px',
+        backgroundColor: '#eef2f3',
+        fontSize: '18px'
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
