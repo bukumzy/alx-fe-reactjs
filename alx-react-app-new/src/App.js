0644ab6814ec3a344890f6e35c0218c5b7d5364e
bukumzy <kumzyd@gmail.com>
2025-11-21 00:23:47 +0100

    new react app 2

--- alx-react-app-new/src/App.js
+++ alx-react-app-new/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+// ⭐ NEW IMPORT
+import Counter from "./components/Counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <UserProfile name="BK" age="29" bio="Frontend developer" />
+      <MainContent />
+
+      {/* ⭐ NEW COMPONENT */}
+      <Counter />
+
+      <Footer />
+    </>
   );
 }
 
