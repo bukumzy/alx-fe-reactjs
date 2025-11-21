03f942f90e4f6811fd2c3da177883c19bbcd73a0
bukumzy <kumzyd@gmail.com>
2025-11-21 01:19:00 +0100

    my company created

---
+++ my-company/src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Contact from "./Contact";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
