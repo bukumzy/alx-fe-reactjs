33e3ee518249936a7dfc046fcb9120a409f056e5
bukumzy <kumzyd@gmail.com>
2025-11-23 20:08:18 +0100

    update 1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,19 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-
-
-export default App
+export default App;
---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,27 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useRecipeStore } from "../recipeStore";
+import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
+
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const recipeId = Number(id);
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === recipeId)
+  );
+
+  const navigate = useNavigate();
+
+  if (!recipe) return <p>Recipe not found</p>;
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipeId} onDelete={() => navigate("/")} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
