4c4fb2ba9b5be94a1b367f1f078130076d1c67bf
Oluwabukunmi Moronfolu <kumzyd@gmail.com>
2026-01-05 11:58:46 +0100

    Chai!

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,11 +1,17 @@
 import { Navigate } from "react-router-dom";
 
-// Simulate authentication
-const isAuthenticated = false; // change to true to test access
+
+function useAuth() {
+  const user = null;
+  return { isAuthenticated: !!user };
+}
 
 export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
   }
+
   return children;
 }
